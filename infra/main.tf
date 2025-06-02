49c3c6c4447bd42331b73d884a51167084402b90
Gabriel <gabrielkuhnen38@gmail.com>
2025-06-01 23:12:50 -0300

    tafarel

--- infra/main.tf
+++ infra/main.tf
@@ -15,48 +15,62 @@ provider "aws" {
 resource "aws_key_pair" "iac_key" {
   key_name   = "iac_key"
   public_key = var.public_key
+
+  lifecycle {
+    prevent_destroy = true  # Evita que a key seja destruída sem querer
+    ignore_changes  = [public_key]  # Ignora alterações no conteúdo da chave pública
+  }
 }
 
 resource "aws_security_group" "iac_sg" {
   name        = "iac_sg"
-description = "Abrir SSH e porta da aplicacao"
+  description = "Abrir SSH e porta da aplicacao"
+
+  lifecycle {
+    prevent_destroy = true  # Evita que o SG seja destruído sem querer
+    ignore_changes  = [description]  # Evita erro caso descrição mude
+  }
+
   ingress {
-  description = "Liberar SSH"
-  from_port   = 22
-  to_port     = 22
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
+    description = "Liberar SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-ingress {
-  description = "Liberar HTTP"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
+  ingress {
+    description = "Liberar HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-ingress {
-  description = "Liberar HTTPS"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
+  ingress {
+    description = "Liberar HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
- description = "Permitir todo trafego de saida"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-}
+    description = "Permitir todo trafego de saida"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "app" {
-  ami                    = var.ubuntu_ami_id          # Ubuntu 22.04 LTS
+  ami                    = var.ubuntu_ami_id
   instance_type          = var.instance_type
   key_name               = aws_key_pair.iac_key.key_name
   vpc_security_group_ids = [aws_security_group.iac_sg.id]
 
-  tags = { Name = "iac-app" }
+  tags = {
+    Name = "iac-app"
+  }
 }
